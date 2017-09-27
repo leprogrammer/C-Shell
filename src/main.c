377a7a1a044f77a1ddc3f5ea518f5293923c614c
Tejas Prasad <leprogrammer@users.noreply.github.com>
2017-09-26 23:45:41 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,11 @@
 #include "sfish.h"
 #include "debug.h"
 
-/*
- * As in previous hws the main function must be in its own file!
- */
 volatile int alarmTime = 0;
 
 int main(int argc, char const *argv[], char* envp[]){
-    /* DO NOT MODIFY THIS. If you do you will get a ZERO. */
     rl_catch_signals = 0;
-    /* This is disable readline's default signal handlers, since you are going to install your own.*/
+    
     char *cmd;
     char *cmdHereDoc;
 
@@ -523,15 +519,13 @@ int main(int argc, char const *argv[], char* envp[]){
         //while((exit = wait(&child_status)) != -1);
         shellLine = shellHeader();
         //printf("%s\n",cmd);
-        /* All your debug print statements should use the macros found in debu.h */
-        /* Use the `make debug` target in the makefile to run with these enabled. */
         //info("Length of command entered: %ld\n", strlen(cmd));
-        /* You WILL lose points if your shell prints out garbage values. */
+        
 
 
     }
 
-    /* Don't forget to free allocated memory, and close file descriptors. */
+    
     free(cmd);
     free(shellLine);
     sigprocmask(SIG_SETMASK, &prev_maskTop, NULL);
